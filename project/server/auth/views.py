c6ce6b51d64e7d7865d3388335e1798810ee46d3
Sebastian Burgstaller <sebastian.burgstaller@gmail.com>
2017-12-06 22:40:01 -0800

    added assay view

--- project/server/auth/views.py
+++ project/server/auth/views.py
@@ -1,13 +1,92 @@
 # project/server/auth/views.py
 
 
-from flask import Blueprint, request, make_response, jsonify
+from flask import Blueprint, request, make_response, jsonify, session
 from flask.views import MethodView
 
 from project.server import bcrypt, db
 from project.server.models import User, BlacklistToken
 
+import pandas as pd
+import wikidataintegrator as wdi
+
 auth_blueprint = Blueprint('auth', __name__)
+
+
+assay_data = pd.read_csv('/Users/sebastianburgstaller/Documents/jupyter-notebooks/calibr_data/reframe_short_20170822.csv')
+gvk_dt = pd.read_csv('/Users/sebastianburgstaller/Documents/jupyter-notebooks/calibr_data/gvk_data_to_release.csv')
+
+
+ikey_wd_map = wdi.wdi_helpers.id_mapper('P235')
+wd_ikey_map = dict(zip(ikey_wd_map.values(), ikey_wd_map.keys()))
+
+print('wd ikey map length:', len(wd_ikey_map))
+
+
+def get_assay_data(qid):
+    tmp_dt = assay_data.loc[assay_data['wikidata'] == qid, :]
+
+    ad = list()
+
+    for c, x in tmp_dt.iterrows():
+        tmp_obj = dict()
+
+        # for k in x.keys():
+        #     tmp_obj.update({k: x[k]})
+
+
+        # only return the data really necessary for being rendered
+        datamode = x['datamode']
+
+        if datamode not in ['DECREASING', 'INCREASING', 'SUPER_ACTIVE']:
+            continue
+        elif datamode == 'DECREASING':
+            tmp_obj.update({'activity_type': 'IC50'})
+        elif datamode == 'INCREASING':
+            tmp_obj.update({'activity_type': 'EC50'})
+        elif datamode == 'SUPER_ACTIVE':
+            tmp_obj.update({'activity_type': 'SUPER ACTIVE'})
+
+
+        tmp_obj.update({'ac50': round(x['ac50'], 10)})
+        tmp_obj.update({'assay_title': x['assay_title']})
+        tmp_obj.update({'smiles': x['smiles']})
+        tmp_obj.update({'PubChem CID': str(x['PubChem CID'])})
+        tmp_obj.update({'wikidata': x['wikidata']})
+        tmp_obj.update({'calibr_id': x['calibr_id']})
+        tmp_obj.update({'inchi_key': x['inchi_key']})
+        tmp_obj.update({'ref': 'Calibr'})
+
+
+        ad.append(tmp_obj)
+
+    return ad
+
+
+def get_gvk_data(qid):
+    ikey = wd_ikey_map[qid]
+    print(ikey)
+
+    tmp_dt = gvk_dt.loc[gvk_dt['ikey'] == ikey, :]
+
+    ad = list()
+
+    for c, x in tmp_dt.iterrows():
+        tmp_obj = dict()
+
+        tmp_obj.update({'drug_name': x['drug_name']})
+        tmp_obj.update({'phase': x['phase'].split('; ')})
+        tmp_obj.update({'drug_roa': x['drug_roa'].split('; ')})
+        tmp_obj.update({'category': x['category'].split('; ')})
+        tmp_obj.update({'mechanism': x['mechanism'].split('; ')})
+        tmp_obj.update({'synonyms': x['synonyms'].split('; ')})
+        tmp_obj.update({'sub_smiles': x['sub_smiles']})
+        tmp_obj.update({'ref': 'GVK'})
+
+        ad.append(tmp_obj)
+
+    return ad
+
 
 
 class RegisterAPI(MethodView):
@@ -18,6 +97,9 @@ class RegisterAPI(MethodView):
     def post(self):
         # get the post data
         post_data = request.get_json()
+        print('thats the post data', post_data, request.values)
+
+
         # check if user already exists
         user = User.query.filter_by(email=post_data.get('email')).first()
         if not user:
@@ -178,11 +260,61 @@ class LogoutAPI(MethodView):
             }
             return make_response(jsonify(responseObject)), 403
 
+
+class AssayDataAPI(MethodView):
+    """
+    Assaydata resource
+    """
+
+    def __init__(self):
+        pass
+
+    def get(self):
+        # get the auth token
+        auth_header = request.headers.get('Authorization')
+
+        print(auth_header)
+        args = request.args
+        qid = args['qid']
+
+        if auth_header:
+            try:
+                auth_token = auth_header.split(" ")[0]
+            except IndexError:
+                responseObject = {
+                    'status': 'fail',
+                    'message': 'Bearer token malformed.'
+                }
+                return make_response(jsonify(responseObject)), 401
+        else:
+            auth_token = ''
+        if auth_token:
+            resp = User.decode_auth_token(auth_token)
+            if not isinstance(resp, str):
+                user = User.query.filter_by(id=resp).first()
+                if user.id:
+                    responseObject = get_assay_data(qid)
+
+                    return make_response(jsonify(responseObject)), 200
+            responseObject = {
+                'status': 'fail',
+                'message': resp
+            }
+            return make_response(jsonify(responseObject)), 401
+        else:
+            responseObject = {
+                'status': 'fail',
+                'message': 'Provide a valid auth token.'
+            }
+            return make_response(jsonify(responseObject)), 401
+
+
 # define the API resources
 registration_view = RegisterAPI.as_view('register_api')
 login_view = LoginAPI.as_view('login_api')
 user_view = UserAPI.as_view('user_api')
 logout_view = LogoutAPI.as_view('logout_api')
+assay_data_view = AssayDataAPI.as_view('assay_data_api')
 
 # add Rules for API Endpoints
 auth_blueprint.add_url_rule(
@@ -204,4 +336,9 @@ auth_blueprint.add_url_rule(
     '/auth/logout',
     view_func=logout_view,
     methods=['POST']
+)
+auth_blueprint.add_url_rule(
+    '/assaydata',
+    view_func=assay_data_view,
+    methods=['GET'],
 )

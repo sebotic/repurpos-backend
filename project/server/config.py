4d94d7702c9e6a1ffa5ebc3966f4e9a6810ad745
Sebastian Burgstaller <sebastian.burgstaller@gmail.com>
2018-02-13 16:15:34 +0100

    added recaptcha

--- project/server/config.py
+++ project/server/config.py
@@ -2,8 +2,8 @@
 
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
-postgres_local_base = 'postgresql://postgres:@localhost/'
-database_name = 'flask_jwt_auth'
+postgres_local_base = 'postgresql://sebastianburgstaller:@localhost/'
+database_name = 'repurpos_db'
 
 
 class BaseConfig:

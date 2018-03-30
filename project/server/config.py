1dbf095377d1f828732d20f7500fafd2c8aa07d1
Sebastian Burgstaller <sebastian.burgstaller@gmail.com>
2018-03-30 17:58:39 +0200

    changed email config

--- project/server/config.py
+++ project/server/config.py
@@ -15,7 +15,7 @@ class BaseConfig:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Mail settings
-    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_SERVER = 'localhost'
     MAIL_PORT = 465
     MAIL_USE_TLS = False
     MAIL_USE_SSL = True
